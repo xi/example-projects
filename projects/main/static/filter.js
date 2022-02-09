04a498ff767c4287651ec02a4eac1a98f8bf77fa
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-02-09 22:09:32 +0100

    Revert "add free text filter"
    This reverts commit 38458852d0cd05f192bf8880e3c91af0276123bd.

--- projects/main/static/filter.js
+++
@@ -1,9 +0,0 @@
-var input = document.querySelector('input');
-var items = Array.from(document.querySelectorAll('[data-filter]'));
-
-input.addEventListener('input', event => {
-    var words = event.target.value.toLowerCase().split(' ');
-    items.forEach(item => {
-        item.hidden = !words.every(word => item.dataset.filter.includes(word));
-    });
-});
