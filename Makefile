62e9cc7294b58d2c7b306d83adf0253925a60ec8
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-02-09 21:33:44 +0100

    setup bootstrap

--- Makefile
+++ Makefile
@@ -11,5 +11,6 @@ run:
 install:
 	python3 -m venv $(VIRTUAL_ENV)
 	$(VIRTUAL_ENV)/bin/pip install -r requirements.txt
+	npm install bootstrap@^5.0.0
 	$(VIRTUAL_ENV)/bin/python manage.py migrate
 	$(VIRTUAL_ENV)/bin/python manage.py loaddata data.json
--- projects/settings/base.py
+++ projects/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
     'projects.main',
 ]
 
@@ -99,7 +100,27 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'npm.finders.NpmFinder',
+]
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+NPM_FILE_PATTERNS = {
+    'bootstrap': [
+        'dist/css/bootstrap.min.css',
+    ],
+}
+
+BOOTSTRAP5 = {
+    'set_placeholder': False,
+}
